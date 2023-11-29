849b5d6910ee9509d39e1bb458823183605fcb20
elianbenjamin <elianbenjamin30@gmail.com>
2023-11-28 21:07:56 -0300

    errorPage

--- client/src/components/ErrorPage/ErrorPage.tsx
+++ client/src/components/ErrorPage/ErrorPage.tsx
@@ -1,8 +1,21 @@
-const ErrorPage = () => {
+
+import styles from './errorPage.module.css'
+import { NavLink } from "react-router-dom";
+
+interface ErrorPageProps {}
+
+const ErrorPage: React.FC<ErrorPageProps> = () => {
     return (
-        <div className="error-page">
-            <h1>Error 404</h1>
+        <div className={styles.container}>
+            <img className={styles.img} src="https://imgs.search.brave.com/396As5bq_qjMgO73IosiTtPQWsw7t8WdxX8nBlb6ZoU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDI0MTQ3/MjguanBn"  alt="Elian" />
+
+            <NavLink to={'/'}>
+
+            <button className={styles.btn}>My Portfolio</button>
+            
+            </NavLink>
         </div>
-    )
+    );
 }
+
 export default ErrorPage;
